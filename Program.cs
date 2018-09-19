050725e40e3edd41715f2fd235d5959579325849
Karla110297 <43396180+Karla110297@users.noreply.github.com>
2018-09-18 20:12:58 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T05_KarlaUrrea_TiempoEjecucion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // suma de los números del 1 al 1,000,000
+            TimeSpan stop;
+            TimeSpan start = new TimeSpan(DateTime.Now.Ticks);
+
+            int suma = 0;
+            for(int i = 1; i <= 1000000; i++)
+            {
+                suma += i;
+            }
+            Console.WriteLine("Suma= " + suma);
+            
+            stop = new TimeSpan(DateTime.Now.Ticks);
+            Console.WriteLine(stop.Subtract(start).TotalMilliseconds);
+            Console.ReadKey();
+
+        }
+    }
+}
